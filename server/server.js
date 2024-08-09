e12553b17c6da1769b53f91b949984fac7619a19
kukudalapraveen <kukudalapraveen@gmail.com>
2024-08-09 11:01:36 +0530

    mongoose connect await added

--- server/server.js
+++ server/server.js
@@ -18,7 +18,7 @@ app.use('/api/auth', authRoutes);
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
+await mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
